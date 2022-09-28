87fc998f927f01c60a2074bcef8d39d67603d60c
robin <robin@rnode.me>
2022-09-28 15:21:24 +0800

    refactor: add the ability to poll for notifications

--- ui/src/services/notification.api.ts
+++ ui/src/services/notification.api.ts
@@ -36,6 +36,9 @@ export const useQueryNotificationRedDot = () => {
   return useSWR<{ inbox: number; achievement: number }>(
     isLogin() ? apiUrl : null,
     request.instance.get,
+    {
+      refreshInterval: 3000,
+    },
   );
 };
 
