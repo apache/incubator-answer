ddb612fac79ad588ca737b469a790d54cd01767f
kumfo <kumfo@sifou.com>
2022-10-28 17:12:22 +0800

    feat: revision repo test

--- internal/service/revision/revision.go
+++ internal/service/revision/revision.go
@@ -9,7 +9,6 @@ import (
 // RevisionRepo revision repository
 type RevisionRepo interface {
 	AddRevision(ctx context.Context, revision *entity.Revision, autoUpdateRevisionID bool) (err error)
-	GetRevision(ctx context.Context, id string) (revision *entity.Revision, exist bool, err error)
 	GetLastRevisionByObjectID(ctx context.Context, objectID string) (revision *entity.Revision, exist bool, err error)
 	GetRevisionList(ctx context.Context, revision *entity.Revision) (revisionList []entity.Revision, err error)
 	UpdateObjectRevisionId(ctx context.Context, revision *entity.Revision, session *xorm.Session) (err error)
