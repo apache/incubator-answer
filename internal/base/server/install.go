784f9631f7df5a6b8bcfa3ee13ae0a20944f12e2
aichy126 <16996097+aichy126@users.noreply.github.com>
2022-11-02 17:30:02 +0800

    update  install api

--- internal/base/server/install.go
+++ internal/base/server/install.go
@@ -2,14 +2,26 @@ package server
 
 import (
 	"embed"
+	"fmt"
+	"io/fs"
 	"net/http"
 
 	"github.com/answerdev/answer/ui"
 	"github.com/gin-gonic/gin"
+	"github.com/segmentfault/pacman/log"
 )
+
+const UIStaticPath = "build/static"
 
 type _resource struct {
 	fs embed.FS
+}
+
+// Open to implement the interface by http.FS required
+func (r *_resource) Open(name string) (fs.File, error) {
+	name = fmt.Sprintf(UIStaticPath+"/%s", name)
+	log.Debugf("open static path %s", name)
+	return r.fs.Open(name)
 }
 
 // NewHTTPServer new http server.
@@ -20,7 +32,10 @@ func NewInstallHTTPServer() *gin.Engine {
 	r.GET("/healthz", func(ctx *gin.Context) { ctx.String(200, "OK??") })
 
 	// gin.SetMode(gin.ReleaseMode)
-	r.StaticFS("/static", http.FS(ui.Build))
+
+	r.StaticFS("/static", http.FS(&_resource{
+		fs: ui.Build,
+	}))
 
 	return r
 }
