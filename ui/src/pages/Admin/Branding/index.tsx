46e4bf84a079f36dd3756fe59f82d3cf35800882
robin <robin@rnode.me>
2022-11-10 16:07:56 +0800

    refactor(ui): Handling details

--- ui/src/pages/Admin/Branding/index.tsx
+++ ui/src/pages/Admin/Branding/index.tsx
@@ -12,24 +12,10 @@ const Index: FC = () => {
     'https://image-static.segmentfault.com/405/057/4050570037-636c7b0609a49',
   );
 
-  const imgUpload = (file: any) => {
-    return new Promise((resolve) => {
-      console.log(file);
-      resolve(true);
-    });
-  };
   return (
     <div>
       <h3 className="mb-4">{t('page_title')}</h3>
-      <BrandUpload
-        type="logo"
-        imgPath={img}
-        uploadCallback={imgUpload}
-        deleteCallback={() => {
-          console.log('delete');
-          setImg('');
-        }}
-      />
+      <BrandUpload type="logo" value={img} onChange={setImg} />
     </div>
   );
 };
