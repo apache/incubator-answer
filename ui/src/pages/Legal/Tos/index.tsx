a5e9225d18cf4d1ac230236ae104a0e761b3aedf
haitao(lj) <haitao@sifou.com>
2022-11-17 19:01:42 +0800

    fix(agreements): #1219

--- ui/src/pages/Legal/Tos/index.tsx
+++ ui/src/pages/Legal/Tos/index.tsx
@@ -4,6 +4,18 @@ import { useLegalTos } from '@/services';
 
 const Index: FC = () => {
   const { data: tos } = useLegalTos();
+  const contentText = tos?.terms_of_service_original_text;
+  let matchUrl: URL | undefined;
+  try {
+    if (contentText) {
+      matchUrl = new URL(contentText);
+    }
+    // eslint-disable-next-line no-empty
+  } catch (ex) {}
+  if (matchUrl) {
+    window.location.replace(matchUrl.toString());
+    return null;
+  }
   return (
     <div
       className="fmt fs-14"
