c3479b70834d91e3694e0ca2d2f848144fb07723
aichy126 <16996097+aichy126@users.noreply.github.com>
2022-11-30 15:11:06 +0800

    add paginator

--- internal/controller/template_render/controller.go
+++ internal/controller/template_render/controller.go
@@ -1,6 +1,8 @@
 package templaterender
 
 import (
+	"math"
+
 	"github.com/answerdev/answer/internal/service"
 	"github.com/google/wire"
 )
@@ -24,4 +26,58 @@ func NewTemplateRenderController(
 		questionService: questionService,
 		userService:     userService,
 	}
+}
+
+// Paginator page
+// page : now page
+// prepage : Number per page
+// nums : Total
+// Returns the contents of the page in the format of 1, 2, 3, 4, and 5. If the contents are less than 5 pages, the page number is returned
+func Paginator(page, prepage int, nums int64) map[string]interface{} {
+
+	var firstpage int //前一页地址
+	var lastpage int  //后一页地址
+	//根据nums总数，和prepage每页数量 生成分页总数
+	totalpages := int(math.Ceil(float64(nums) / float64(prepage))) //page总数
+	if page > totalpages {
+		page = totalpages
+	}
+	if page <= 0 {
+		page = 1
+	}
+	var pages []int
+	switch {
+	case page >= totalpages-5 && totalpages > 5: //最后5页
+		start := totalpages - 5 + 1
+		firstpage = page - 1
+		lastpage = int(math.Min(float64(totalpages), float64(page+1)))
+		pages = make([]int, 5)
+		for i, _ := range pages {
+			pages[i] = start + i
+		}
+	case page >= 3 && totalpages > 5:
+		start := page - 3 + 1
+		pages = make([]int, 5)
+		firstpage = page - 3
+		for i, _ := range pages {
+			pages[i] = start + i
+		}
+		firstpage = page - 1
+		lastpage = page + 1
+	default:
+		pages = make([]int, int(math.Min(5, float64(totalpages))))
+		for i, _ := range pages {
+			pages[i] = i + 1
+		}
+		firstpage = int(math.Max(float64(1), float64(page-1)))
+		lastpage = page + 1
+		//fmt.Println(pages)
+	}
+	paginatorMap := make(map[string]interface{})
+	paginatorMap["pages"] = pages
+	paginatorMap["totalpages"] = totalpages
+	paginatorMap["firstpage"] = firstpage
+	paginatorMap["lastpage"] = lastpage
+	paginatorMap["currpage"] = page
+	return paginatorMap
 }
