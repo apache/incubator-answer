8acc3c937d98aca1645aa94a4288165fc44a10e9
haitao(lj) <haitao@sifou.com>
2022-12-13 11:46:51 +0800

    feat(admin/login): Forced login blocking development completed

--- ui/src/utils/guard.ts
+++ ui/src/utils/guard.ts
@@ -26,6 +26,7 @@ export type TGuardResult = {
   ok: boolean;
   redirect?: string;
 };
+export type TGuardFunc = () => TGuardResult;
 
 export const deriveLoginState = (): TLoginState => {
   const ls: TLoginState = {
@@ -55,8 +56,18 @@ export const deriveLoginState = (): TLoginState => {
   if (ls.isNormal && user.is_admin === true) {
     ls.isAdmin = true;
   }
-
   return ls;
+};
+
+const isIgnoredPath = (ignoredPath: string | string[]) => {
+  if (!Array.isArray(ignoredPath)) {
+    ignoredPath = [ignoredPath];
+  }
+  const { pathname } = window.location;
+  const matchingPath = ignoredPath.find((_) => {
+    return pathname.indexOf(_) !== -1;
+  });
+  return !!matchingPath;
 };
 
 let pullLock = false;
@@ -129,7 +140,7 @@ export const activated = () => {
 };
 
 export const forbidden = () => {
-  const gr = logged();
+  const gr: TGuardResult = { ok: true };
   const us = deriveLoginState();
   if (gr.ok && !us.isForbidden) {
     gr.ok = false;
@@ -165,6 +176,34 @@ export const allowNewRegistration = () => {
     gr.ok = false;
     gr.redirect = RouteAlias.home;
   }
+  return gr;
+};
+
+export const shouldLoginRequired = () => {
+  const gr: TGuardResult = { ok: true };
+  const loginSetting = loginSettingStore.getState().login;
+  if (!loginSetting.login_required) {
+    return gr;
+  }
+  const us = deriveLoginState();
+  if (us.isLogged) {
+    return gr;
+  }
+  if (
+    isIgnoredPath([
+      RouteAlias.login,
+      RouteAlias.register,
+      '/users/account-recovery',
+      'users/change-email',
+      'users/account-activation/success',
+      '/users/account-activation/failed',
+      '/users/confirm-new-email',
+    ])
+  ) {
+    return gr;
+  }
+  gr.ok = false;
+  gr.redirect = RouteAlias.login;
   return gr;
 };
 
@@ -219,8 +258,7 @@ export const initAppSettingsStore = async () => {
 };
 
 export const shouldInitAppFetchData = () => {
-  const { pathname } = window.location;
-  if (pathname === '/install') {
+  if (isIgnoredPath('/install')) {
     return false;
   }
 
@@ -234,7 +272,6 @@ export const setupApp = async () => {
    * 2. must pre init app settings for app render
    */
   // TODO: optimize `initAppSettingsStore` by server render
-
   if (shouldInitAppFetchData()) {
     await Promise.allSettled([pullLoggedUser(), initAppSettingsStore()]);
     setupAppLanguage();
