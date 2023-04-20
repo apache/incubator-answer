d02ec9712a6cf3dee1b972a90f1edf7056df3e10
LinkinStars <linkinstar@foxmail.com>
2023-04-20 10:04:43 +0800

    fix(rank): fix the wrong reputation authority configuration

--- internal/migrations/v8.go
+++ internal/migrations/v8.go
@@ -63,10 +63,10 @@ func addRolePinAndHideFeatures(x *xorm.Engine) error {
 	}
 
 	defaultConfigTable := []*entity.Config{
-		{ID: 119, Key: "question.pin", Value: `0`},
-		{ID: 120, Key: "question.unpin", Value: `0`},
-		{ID: 121, Key: "question.show", Value: `0`},
-		{ID: 122, Key: "question.hide", Value: `0`},
+		{ID: 119, Key: "question.pin", Value: `-1`},
+		{ID: 120, Key: "question.unpin", Value: `-1`},
+		{ID: 121, Key: "question.show", Value: `-1`},
+		{ID: 122, Key: "question.hide", Value: `-1`},
 	}
 	for _, c := range defaultConfigTable {
 		exist, err := x.Get(&entity.Config{ID: c.ID, Key: c.Key})
