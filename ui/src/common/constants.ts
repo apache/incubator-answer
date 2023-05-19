d6b758993cd6f6797b1cbce0171436e8989e4eba
haitaoo <haitao@sifou.com>
2023-05-19 11:16:46 +0800

    feat(QuestionList): Remember how to sort the list of questions in local storage

--- ui/src/common/constants.ts
+++ ui/src/common/constants.ts
@@ -8,6 +8,8 @@ export const CAPTCHA_CODE_STORAGE_KEY = '_a_captcha_';
 export const DRAFT_QUESTION_STORAGE_KEY = '_a_dq_';
 export const DRAFT_ANSWER_STORAGE_KEY = '_a_da_';
 export const DRAFT_TIMESIGH_STORAGE_KEY = '|_a_t_s_|';
+export const QUESTIONS_ORDER_STORAGE_KEY = '_a_qok_';
+
 export const USER_AGENT_NAMES = {
   SegmentFault: 'SegmentFault',
   WeChat: 'WeChat',
