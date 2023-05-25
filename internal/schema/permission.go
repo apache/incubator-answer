3428b5fbc3b62c00747627d4f9320f0e21787279
LinkinStars <linkinstar@foxmail.com>
2023-05-25 15:56:58 +0800

    feat(permission): add user permission API

--- internal/schema/permission.go
+++ internal/schema/permission.go
@@ -1,11 +1,27 @@
 package schema
 
-const PermissionMemberActionTypeEdit = "edit"
-const PermissionMemberActionTypeReason = "reason"
+import (
+	"strings"
+
+	"github.com/answerdev/answer/internal/base/validator"
+)
 
 // PermissionMemberAction permission member action
 type PermissionMemberAction struct {
 	Action string `json:"action"`
 	Name   string `json:"name"`
 	Type   string `json:"type"`
+}
+
+// GetPermissionReq get permission request
+type GetPermissionReq struct {
+	Action  string   `form:"action"`
+	Actions []string `validate:"omitempty" form:"actions"`
+}
+
+func (r *GetPermissionReq) Check() (errField []*validator.FormErrorField, err error) {
+	if len(r.Action) > 0 {
+		r.Actions = strings.Split(r.Action, ",")
+	}
+	return nil, nil
 }
