b75c4115afa6acd3531da9257c48c877f24abab7
haitaoo <haitao@sifou.com>
2023-07-10 10:34:15 +0800

    fix(Img): Correctly handle `onerror` issues for images where the `src` address changes.

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -12,24 +12,37 @@ const root = ReactDOM.createRoot(
 
 /**
  * Uniformly hide broken images
+ *  - The `onload` event for elements such as `img` can only be `capture` on `document` (window cannot).
+ *  - For images with an empty `src` attribute, sometimes the browser will simply display the broken image without reporting an 'error' event.
  */
-document.addEventListener(
-  'error',
-  (err) => {
-    const { target } = err;
-    if (target === null || !(target instanceof Element)) {
-      return;
-    }
+const handleImgLoad = (evt: Event | UIEvent) => {
+  const { target } = evt;
 
-    if (/IMG/.test(target.nodeName)) {
-      const altText = target.getAttribute('alt')?.trim();
-      if (!altText) {
-        target.classList.add('invisible');
-      }
+  if (target === null || !(target instanceof Element)) {
+    return;
+  }
+  if (!/IMG/i.test(target.nodeName)) {
+    return;
+  }
+
+  if (/error/i.test(evt.type)) {
+    target.classList.add('broken');
+    const attrSrc = target.getAttribute('src');
+    const attrAlt = target.getAttribute('alt')?.trim();
+    // Images without the `src` attribute are hidden directly by `css`.
+    // Images with `alt` content are not hidden - the display of the `alt` content is also hidden.
+    if (attrSrc && !attrAlt) {
+      target.classList.add('invisible');
     }
-  },
-  true,
-);
+  }
+
+  if (/load/i.test(evt.type)) {
+    target.classList.remove('broken', 'invisible');
+  }
+};
+
+document.addEventListener('error', handleImgLoad, true);
+document.addEventListener('load', handleImgLoad, true);
 
 root.render(
   <React.StrictMode>
