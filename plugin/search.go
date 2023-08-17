be5231671fc71b91fe52767ad8a4176195bc8321
LinkinStars <linkinstar@foxmail.com>
2023-08-17 11:30:44 +0800

    feat(search): Add search sync function

--- plugin/search.go
+++ plugin/search.go
@@ -2,6 +2,7 @@ package plugin
 
 import (
 	"context"
+	"github.com/answerdev/answer/internal/entity"
 )
 
 type SearchResult struct {
@@ -83,11 +84,17 @@ const (
 
 type Search interface {
 	Base
+	RegisterSyncer(ctx context.Context, syncer SearchSyncer)
 	SearchContents(ctx context.Context, cond *SearchBasicCond) (res []SearchResult, total int64, err error)
 	SearchQuestions(ctx context.Context, cond *SearchBasicCond) (res []SearchResult, total int64, err error)
 	SearchAnswers(ctx context.Context, cond *SearchBasicCond) (res []SearchResult, total int64, err error)
 	UpdateContent(ctx context.Context, contentID string, content *SearchContent) error
 	DeleteContent(ctx context.Context, contentID string) error
+}
+
+type SearchSyncer interface {
+	GetAnswersPage(ctx context.Context, page, pageSize int) (answerList []*entity.Answer, total int64, err error)
+	GetQuestionsPage(ctx context.Context, page, pageSize int) (questionList []*entity.Question, total int64, err error)
 }
 
 var (
