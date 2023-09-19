541c35e82737fb5fd67900c1d62a79d5d6d3a853
buttercannfly <36593447+buttercannfly@users.noreply.github.com>
2023-09-19 15:30:56 +0800

    fix(image): support upload jog images

--- pkg/checker/file_type.go
+++ pkg/checker/file_type.go
@@ -12,12 +12,14 @@ func IsSupportedImageFile(file io.Reader, ext string) bool {
 	ext = strings.TrimPrefix(ext, ".")
 	var err error
 	switch strings.ToUpper(ext) {
-	case "JPG", "JPEG":
+	case "JPEG":
 		_, err = jpeg.Decode(file)
 	case "PNG":
 		_, err = png.Decode(file)
 	case "ICO":
 		// TODO: There is currently no good Golang library to parse whether the image is in ico format.
+		return true
+	case "JPG":
 		return true
 	default:
 		return false
