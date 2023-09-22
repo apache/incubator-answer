b2d5781633dc0a40cadb3c0a7e8f03ddbb5b79cf
LinkinStars <linkinstar@foxmail.com>
2023-09-22 14:36:40 +0800

    feat(user): support upload webp image

--- pkg/checker/file_type.go
+++ pkg/checker/file_type.go
@@ -1,6 +1,7 @@
 package checker
 
 import (
+	"golang.org/x/image/webp"
 	"image"
 	_ "image/gif" // use init to support decode jpeg,jpg,png,gif
 	_ "image/jpeg"
@@ -9,16 +10,19 @@ import (
 	"strings"
 )
 
-// IsSupportedImageFile currently answers support image type is `image/jpeg,image/jpg,image/png, image/gif`
+// IsSupportedImageFile currently answers support image type is
+// `image/jpeg, image/jpg, image/png, image/gif, image/webp`
 func IsSupportedImageFile(file io.Reader, ext string) bool {
-	ext = strings.TrimPrefix(ext, ".")
+	ext = strings.ToLower(strings.TrimPrefix(ext, "."))
 	var err error
-	switch strings.ToUpper(ext) {
-	case "JPG", "JPEG", "PNG", "GIF": // only allow for `image/jpeg,image/jpg,image/png, image/gif`
+	switch ext {
+	case "jpg", "jpeg", "png", "gif": // only allow for `image/jpeg,image/jpg,image/png, image/gif`
 		_, _, err = image.Decode(file)
-	case "ICO":
+	case "ico":
 		// TODO: There is currently no good Golang library to parse whether the image is in ico format.
 		return true
+	case "webp":
+		_, err = webp.Decode(file)
 	default:
 		return false
 	}
