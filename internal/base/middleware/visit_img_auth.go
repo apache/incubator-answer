a0d57a18903f58994c43e976018e2605b2dc8c00
LinkinStars <linkinstar@foxmail.com>
2023-10-10 17:49:20 +0800

    feat(visit): can visit branding image without token

--- internal/base/middleware/visit_img_auth.go
+++ internal/base/middleware/visit_img_auth.go
@@ -4,11 +4,18 @@ import (
 	"github.com/answerdev/answer/internal/base/constant"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 // VisitAuth when user visit the site image, check visit token. This only for private mode.
 func (am *AuthUserMiddleware) VisitAuth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		// If visit brand image, no need to check visit token. Because the brand image is public.
+		if strings.HasPrefix(ctx.Request.URL.Path, "/uploads/branding/") {
+			ctx.Next()
+			return
+		}
+
 		siteLogin, err := am.siteInfoCommonService.GetSiteLogin(ctx)
 		if err != nil {
 			return
