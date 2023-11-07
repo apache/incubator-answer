56e0756037b1569cb85713a7cca84a02c67d4ff7
kelvinkuo <kelvinkuo224@gmail.com>
2023-11-07 18:17:25 +0800

    fix issue-609

--- ui/src/pages/Questions/Ask/index.tsx
+++ ui/src/pages/Questions/Ask/index.tsx
@@ -383,7 +383,10 @@ const Ask = () => {
                     return (
                       <option key={`${create_at}`} value={index}>
                         {`${date} - ${user_info.display_name} - ${
-                          reason || t('default_reason')
+                          reason ||
+                          (index === revisions.length - 1
+                            ? t('default_first_reason')
+                            : t('default_reason'))
                         }`}
                       </option>
                     );
