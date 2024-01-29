a24b22ce5ada0707b6a994bda205e58186d3c97e
shuai <lishuailing@sifou.com>
2024-01-29 15:40:33 +0800

    fix: comment sort

--- ui/src/services/common.ts
+++ ui/src/services/common.ts
@@ -62,7 +62,7 @@ export const useQueryComments = (params) => {
     params.page = 1;
   } else {
     // only first page need commentId
-    params.query_cond = 'created_at';
+    params.query_cond = '';
     delete params.comment_id;
   }
   return useSWR<Type.ListResult>(
