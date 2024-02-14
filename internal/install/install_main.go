1b1e3d1a4f79974ab247219eb43e5559fc4ced4f
Justman10000 <justmegaliga10000@gmail.com>
2024-02-14 16:11:19 +0100

    Use the ip instead of simply localhost to access answer

--- internal/install/install_main.go
+++ internal/install/install_main.go
@@ -21,6 +21,7 @@ package install
 
 import (
 	"fmt"
+    "net"
 	"os"
 
 	"github.com/apache/incubator-answer/internal/base/translator"
@@ -31,6 +32,29 @@ var (
 	port     = os.Getenv("INSTALL_PORT")
 	confPath = ""
 )
+
+func getLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    for _, addr := range addrs {
+        var ip net.IP
+        switch v := addr.(type) {
+        case *net.IPNet:
+            ip = v.IP
+        case *net.IPAddr:
+            ip = v.IP
+        }
+
+        if ip != nil && !ip.IsLoopback() && ip.To4() != nil {
+            return ip.String()
+        }
+    }
+    return ""
+}
 
 func Run(configPath string) {
 	confPath = configPath
@@ -50,8 +74,10 @@ func Run(configPath string) {
 		port = "80"
 	}
 
+	localIP := getLocalIP()
+
 	if (installByEnv, err := TryToInstallByEnv(); !installByEnv) {
-		fmt.Printf("[SUCCESS] Answer installation service will run at: http://localhost:%s/install/ \n", port)
+		fmt.Printf("[SUCCESS] Answer installation service will run at: http://%s:%s/install/ \n", localIP, port)
 		if err = installServer.Run(":" + port); err != nil {
 			panic(err)
 		}
