f89e946d1ddd0e7cdfbcfaaea2da294807538e72
LinkinStars <linkinstar@foxmail.com>
2024-03-20 11:53:22 +0800

    feat(migration): update clarity description config

--- internal/migrations/v20.go
+++ internal/migrations/v20.go
@@ -21,11 +21,18 @@ package migrations
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/apache/incubator-answer/internal/entity"
+	"github.com/segmentfault/pacman/log"
 	"xorm.io/xorm"
 )
 
 func addReview(ctx context.Context, x *xorm.Engine) error {
+	c := &entity.Config{Key: "reason.not_clarity", Value: `{"name":"needs details or clarity","description":"This question currently includes multiple questions in one. It should focus on one problem only."}`}
+	if _, err := x.Context(ctx).Update(c, &entity.Config{Key: "reason.not_clarity"}); err != nil {
+		log.Errorf("update %+v config failed: %s", c, err)
+		return fmt.Errorf("update config failed: %w", err)
+	}
 	return x.Context(ctx).Sync(new(entity.Review))
 }
