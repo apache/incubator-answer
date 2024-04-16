8497f6b65e80d8d160a2c7473312da727d25e643
robin <robin@rnode.me>
2024-04-16 15:48:30 +0800

    refactor(ui): Refactor editor theme and add line wrapping functionality

--- ui/src/components/Editor/utils/index.ts
+++ ui/src/components/Editor/utils/index.ts
@@ -83,7 +83,6 @@ export const useEditor = ({
     const theme = EditorView.theme(
       {
         '&': {
-          width: '100%',
           height: '100%',
         },
         '&.cm-focused': {
@@ -111,6 +110,7 @@ export const useEditor = ({
         }),
         theme,
         placeholder(placeholderText),
+        EditorView.lineWrapping,
       ],
     });
 
