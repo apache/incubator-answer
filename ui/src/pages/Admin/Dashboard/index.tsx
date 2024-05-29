9b2277d9addd93a684e93435bbabef24549d8130
shuai <lishuailing@sifou.com>
2024-05-29 15:24:05 +0800

    fix: fix translation error

--- ui/src/pages/Admin/Dashboard/index.tsx
+++ ui/src/pages/Admin/Dashboard/index.tsx
@@ -21,7 +21,6 @@ import { FC } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
-import { siteInfoStore } from '@/stores';
 import { useDashBoard } from '@/services';
 
 import {
@@ -33,7 +32,6 @@ import {
 
 const Dashboard: FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'admin.dashboard' });
-  const { name: siteName } = siteInfoStore((_) => _.siteInfo);
   const { data } = useDashBoard();
 
   if (!data) {
