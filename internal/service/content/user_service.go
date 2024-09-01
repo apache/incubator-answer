63e1da69a1b1dea1a026f7714a2b26e8356e9fbf
alvin <bioluminescencecloud@gmail.com>
2024-09-01 21:32:19 +0800

    refactor: add err handling for activeUser

--- internal/service/content/user_service.go
+++ internal/service/content/user_service.go
@@ -527,6 +527,7 @@ func (us *UserService) UserVerifyEmail(ctx context.Context, req *schema.UserVeri
 	}
 	if err = us.userActivity.UserActive(ctx, userInfo.ID); err != nil {
 		log.Error(err)
+		return nil, err
 	}
 
 	// In the case of three-party login, the associated users are bound
@@ -660,6 +661,7 @@ func (us *UserService) UserChangeEmailVerify(ctx context.Context, content string
 	if userInfo.MailStatus == entity.EmailStatusToBeVerified {
 		if err = us.userActivity.UserActive(ctx, userInfo.ID); err != nil {
 			log.Error(err)
+			return nil, err
 		}
 	}
 
