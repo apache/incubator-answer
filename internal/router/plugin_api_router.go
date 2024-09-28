4c9e37fa6e2af4a6cd6bf41ccea0cdb0634210ec
Anan <1449891717@qq.com>
2024-09-28 05:07:54 +0000

    Add importer plugin type

--- internal/router/plugin_api_router.go
+++ internal/router/plugin_api_router.go
@@ -30,6 +30,7 @@ type PluginAPIRouter struct {
 	captchaController    *controller.CaptchaController
 	embedController      *controller.EmbedController
 	renderController     *controller.RenderController
+	importerController   *controller.ImporterController
 }
 
 func NewPluginAPIRouter(
@@ -38,6 +39,7 @@ func NewPluginAPIRouter(
 	captchaController *controller.CaptchaController,
 	embedController *controller.EmbedController,
 	renderController *controller.RenderController,
+	importerController *controller.ImporterController,
 ) *PluginAPIRouter {
 	return &PluginAPIRouter{
 		connectorController:  connectorController,
@@ -68,6 +70,9 @@ func (pr *PluginAPIRouter) RegisterUnAuthConnectorRouter(r *gin.RouterGroup) {
 	r.GET("/captcha/config", pr.captchaController.GetCaptchaConfig)
 	r.GET("/embed/config", pr.embedController.GetEmbedConfig)
 	r.GET("/render/config", pr.renderController.GetRenderConfig)
+
+	// importer plugin
+	r.POST("/importer/answer", pr.importerController.ImportCommand)
 }
 
 func (pr *PluginAPIRouter) RegisterAuthUserConnectorRouter(r *gin.RouterGroup) {
