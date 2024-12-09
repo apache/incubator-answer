5274d3cffd23aa047c339dd0df65dfce421ee94b
LinkinStars <linkinstar@foxmail.com>
2024-12-09 17:07:38 +0800

    fix(migrations): set v1.4.2 migration to required clean cache

--- internal/migrations/migrations.go
+++ internal/migrations/migrations.go
@@ -99,7 +99,7 @@ var migrations = []Migration{
 	NewMigration("v1.3.6", "add hot score to question table", addQuestionHotScore, true),
 	NewMigration("v1.4.0", "add badge/badge_group/badge_award table", addBadges, true),
 	NewMigration("v1.4.1", "add question link", addQuestionLink, true),
-	NewMigration("v1.4.2", "add the number of question links", addQuestionLinkedCount, false),
+	NewMigration("v1.4.2", "add the number of question links", addQuestionLinkedCount, true),
 }
 
 func GetMigrations() []Migration {
